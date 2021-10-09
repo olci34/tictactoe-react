4b9ec19617cc3bfc5fe96cc47a30be48635b67ef
olci34 <developerogulcan@gmail.com>
2021-10-08 21:06:43 -0400

    convert Board class to function component

--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import Square from "./Square";
-class Board extends Component {
-  render() {
-    return (
-      <div id="board">
-        {this.props.squares.map((square, index) => (
-          <Square key={index} value={square} onClick={() => this.props.handleClick(index)} />
-        ))}
-      </div>
-    );
-  }
+const Board = (props) => {
+  return (
+    <div id="board">
+      {props.squares.map((square, index) => (
+        <Square key={index} value={square} onClick={() => props.handleClick(index)} />
+      ))}
+    </div>
+  ) 
 }
+// class Board extends Component {
+//   render() {
+//     return (
+//       <div id="board">
+//         {this.props.squares.map((square, index) => (
+//           <Square key={index} value={square} onClick={() => this.props.handleClick(index)} />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
 
 export default Board;
